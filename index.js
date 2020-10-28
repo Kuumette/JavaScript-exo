ffed92384b7a7dc7db1d3335d47694d297745683
Manon <ballatoremanon@gmail.com>
2020-10-28 15:48:24 +0100

    Cour Javascript exercices

---
+++ index.js
@@ -0,0 +1,53 @@
+//var number = 123;
+//var bool = true;
+//var text = "me";
+//var obj = new Date();
+/*
+console.log(typeof number);
+console.log(typeof bool);
+console.log(typeof text);
+console.log(typeof obj);
+/*Message dalerte avant d'aficher la page
+alert("coucou");*/
+/*ouvrire un alerte avec un texte a rentré
+prompt("coucou");*/
+
+//var bool = false
+//var bool = 0;
+//var bool = "";
+//var bool = null;
+//var bool = undefined
+//var bool = NaN;
+//console.log(Boolean(bool));
+
+//var testArray = ["Manon", 1, true];
+/*
+NE PAS SENS SERVIR
+testArray[0] = "Manon"
+testArray[1] = "1"
+testArray[2] = "true"
+*/
+//console.log(testArray);
+//console.log(typeof testArray);
+/*
+var person = {
+    firstname: "iuv",
+    lastname: "ffff",
+    age: 25
+};*/
+/*Methode longue
+person.firstname = "iuv";
+person.lastname = "ffff"
+*/
+//console.log(`Je suis ${person.firstname} mon age et de ${person.age}`);
+//console.log(person["firstname"]);
+
+var testArray = ["fraise", "orange", "melon", "banane", "framboise"];
+var fruit={couleur:"vert_claire",forme:"rond",nom:"melon",gout:"sucré"};
+/*NE PAS SENS SERVIR
+testArray[0] = "Manon"
+testArray[1] = "1"
+testArray[2] = "true"
+*/
+console.log(testArray[2]);
+console.log(fruit);
